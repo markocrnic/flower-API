572513e053587495adb4d180fd6e1abedf50331b
markocrnic <markons996@gmail.com>
2019-11-05 16:03:00 +0100

    Added Jaeger tracing to all API's

--- app/interface.py
+++ app/interface.py
@@ -1,43 +1,56 @@
 from flask import Flask, request
 from flask_cors import CORS
+from api_management.jaeger import initializejaeger
+from flask_opentracing import FlaskTracing
 import implementation as implementation
 
 
 app = Flask(__name__)
 CORS(app)
 
+jaeger_tracer = initializejaeger()
+tracing = FlaskTracing(jaeger_tracer)
+
 
 @app.route('/flowers/', methods=['GET', 'POST'])
+@tracing.trace()
 def flowersGlobal():
-    try:
-        if request.method == 'GET':
-            data = implementation.getAllFlowers()
-            return data
-        elif request.method == 'POST':
-            try:
-                schema = implementation.getSchema()
-                schema.validate(request.json)
-            except:
-                return {'msg': 'Data is not valid.'}, 403
-            return implementation.postFlower(request)
-    except:
-        return {'msg': 'Something went wrong at /flowers/'}, 500
+    with jaeger_tracer.start_active_span(
+            'Flowers-API endpoint /flowers/') as scope:
+        scope.span.log_kv({'event': 'Calling endpoint /flowers/', 'request_method': request.method})
+        try:
+            if request.method == 'GET':
+                data = implementation.getAllFlowers()
+                return data
+            elif request.method == 'POST':
+                try:
+                    schema = implementation.getSchema()
+                    schema.validate(request.json)
+                except:
+                    return {'msg': 'Data is not valid.'}, 403
+                return implementation.postFlower(request)
+        except:
+            return {'msg': 'Something went wrong at /flowers/'}, 500
 
 
 @app.route('/flowers/<int:flower_id>/', methods=['GET', 'PUT', 'DELETE'])
+@tracing.trace()
 def flowersWithID(flower_id):
-    try:
-        if request.method == 'GET':
-            return implementation.getFlowerByID(flower_id)
-        elif request.method == 'PUT':
-            return implementation.putFlowerByID(request, flower_id)
-        elif request.method == 'DELETE':
-            return implementation.deleteFlowerByID(flower_id)
-        else:
-            return {"msg": "Check request again."}, 403
-    except Exception as e:
-        print(e)
-        return {"msg": "Something went wrong at /flowers/<int:flower_id>"}, 500
+    with jaeger_tracer.start_active_span(
+            'Flowers-API endpoint /flowers/<int:flower_id>/') as scope:
+        scope.span.log_kv({'event': 'Calling endpoint /flowers/<int:flower_id>/', 'request_method': request.method})
+        try:
+            if request.method == 'GET':
+                return implementation.getFlowerByID(flower_id)
+            elif request.method == 'PUT':
+                return implementation.putFlowerByID(request, flower_id)
+            elif request.method == 'DELETE':
+                return implementation.deleteFlowerByID(flower_id)
+            else:
+                return {"msg": "Check request again."}, 403
+        except Exception as e:
+            print(e)
+            return {"msg": "Something went wrong at /flowers/<int:flower_id>"}, 500
 
 
 if __name__ == '__main__':
